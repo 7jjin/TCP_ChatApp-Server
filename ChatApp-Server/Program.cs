f11f7d7401bf4bc5df4073b2ecd3d5d26c9acfa1
진형 조 <wlsgud6305@naver.com>
2024-12-02 10:03:09 +0900

    초기 커밋.

---
+++ ChatApp-Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_Server
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Server server = new Server();
+            server.StartServer(5000); // 서버 포트: 5000
+        }
+    }
+}
---
+++ ChatApp-Server/Utils/PasswordHasher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp_Server.Utils
+{
+    internal class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(password);
+                var hashBytes = sha256.ComputeHash(passwordBytes);
+                return Convert.ToBase64String(hashBytes); // Base64 문자열로 변환
+            }
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashedInput = HashPassword(password); // 입력 비밀번호를 해시화
+            return hashedInput == hashedPassword;     // 데이터베이스 값과 비교
+        }
+    }
+}
